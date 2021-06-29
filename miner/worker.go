08d00fce1530d57a4064aca2274cd44061f3f378
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-06-29 15:08:30 -0700

    only write block to state if not nevm on miner
    miner node should remain idempotent

--- miner/worker.go
+++ miner/worker.go
@@ -626,11 +626,14 @@ func (w *worker) resultLoop() {
 				}
 				logs = append(logs, receipt.Logs...)
 			}
-			// Commit block and state to database.
-			_, err := w.chain.WriteBlockWithState(block, receipts, logs, task.state, true)
-			if err != nil {
-				log.Error("Failed writing block to chain", "err", err)
-				continue
+			// SYSCOIN
+			if !w.chainConfig.IsPolygon(block.Header().Number) {
+				// Commit block and state to database.
+				_, err := w.chain.WriteBlockWithState(block, receipts, logs, task.state, true)
+				if err != nil {
+					log.Error("Failed writing block to chain", "err", err)
+					continue
+				}
 			}
 			log.Info("Successfully sealed new block", "number", block.Number(), "sealhash", sealhash, "hash", hash,
 				"elapsed", common.PrettyDuration(time.Since(task.createdAt)))
