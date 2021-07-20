968bcee57dd0e789c841525301b3350303a0b4e9
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-19 21:47:00 -0700

    put descriptor in base datadir where it belongs

--- cmd/puppeth/module_explorer.go
+++ cmd/puppeth/module_explorer.go
@@ -39,7 +39,8 @@ RUN mkdir ~/.syscoin
 	RUN mkdir ~/.syscoin/regtest
 {{end}}
 RUN apk add --no-cache wget
-RUN wget https://raw.githubusercontent.com/syscoin/descriptors/{{if eq .NetworkID 58}}testnet{{else}}master{{end}}/gethdescriptor.json -O ~/.syscoin/{{if eq .NetworkID 58}}regtest{{end}}/gethdescriptor.json
+RUN wget https://raw.githubusercontent.com/syscoin/descriptors/{{if eq .NetworkID 58}}testnet{{else}}master{{end}}/gethdescriptor.json -O ~/.syscoin/gethdescriptor.json
+
 ENV SYSCOIN_VERSION=4.3.99
 ENV SYSCOIN_PREFIX=/opt/syscoin-${SYSCOIN_VERSION}
 COPY --from=geth-alpine /usr/local/bin/geth ~/.syscoin/sysgeth
