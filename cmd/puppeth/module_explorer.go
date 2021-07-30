b4e0d69d404d9bb98cc39533725838c2dd5e7c82
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-07-30 12:35:16 -0700

    flip supportChains logic

--- cmd/puppeth/module_explorer.go
+++ cmd/puppeth/module_explorer.go
@@ -161,12 +161,12 @@ func deployExplorer(client *sshClient, network string, bootnodes []string, confi
 	subNetwork := ""
 	showPriceChart := "true"
 	disableExchangeRates := "false"
-	supportedChains := "[ { \"title\": \"Tanenbaum\", \"url\": \"https://blockscout.com/rsk/mainnet\", \"test_net?\": true } ]"
+	supportedChains := "[ { \"title\": \"Syscoin Mainnet\", \"url\": \"https://blockscout.com/rsk/mainnet\" } ]"
 	if config.node.network == 58 {
 		subNetwork = "Tanenbaum"
 		disableExchangeRates = "false"
 		showPriceChart = "true"
-		supportedChains = "[ { \"title\": \"Syscoin Mainnet\", \"url\": \"https://blockscout.com/rsk/mainnet\" } ]"
+		supportedChains = "[ { \"title\": \"Tanenbaum\", \"url\": \"https://blockscout.com/rsk/mainnet\", \"test_net?\": true } ]"
 	}
 	host := config.host
 	if host == "" {
